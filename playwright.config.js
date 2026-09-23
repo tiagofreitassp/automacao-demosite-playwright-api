9a67b6ae3f1a15c12f2114b0da07e325f62f23ac
Tiago Freitas <tiago.freitas.sp@gmail.com>
2026-09-23 14:32:11 -0300

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -14,16 +14,24 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  timeout: 15 * 1000,//15 segundos o Timeout para cada teste concluir a execução
+  expect: {
+    timeout: 15 * 1000,//15 segundos para o timeout da assertiva
+  },
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,//Se o teste falhar, executar novamente. Tentar novamente até 3 vezes.
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { outputFolder: 'playwright-report/html', open: 'never' }],
+    ['list', { printSteps: true }],
+    ['json', { outputFile: 'test-results/results.json' }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -31,13 +39,25 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    timezoneId: 'America/Sao_Paulo',
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
+      name: 'edge',
+      use: {
+        // Supported Microsoft Edge channels are: msedge, msedge-beta, msedge-dev, msedge-canary
+        channel: 'msedge',
+      },
+    },
+
+    /*
+    {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'],
+        viewport: {width: 1280, height: 720},//O Playwright não trabalha com Maximizar
+      },
     },
 
     {
